1d39c181266685cc726f268ccb0cffe4b41eaa82
Haris Sabanovic <hasa2303@student.miun.se>
2024-05-19 13:11:23 +0200

    skapade routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,6 @@ import { Routes } from '@angular/router';
 import { CoursesComponent } from './courses/courses.component';
 
 export const routes: Routes = [
-    { path: "courses", component: CoursesComponent }
+    { path: "courses", component: CoursesComponent },
+    { path: "", redirectTo: "courses", pathMatch: "full"}
 ];
